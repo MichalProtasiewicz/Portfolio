05f0a616616155504a47290a99d74eecb4d4aa7d
Michał Protasiewicz <protez95@gmail.com>
2020-09-19 19:00:42 +0200

    Update Paragraph.js

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
   font-size: ${({ theme }) => theme.fontSize.xs};
   font-weight: ${({ theme }) => theme.regular};
+  @media (min-width: 1400px) {
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
 `;
 
 export default Paragraph;
