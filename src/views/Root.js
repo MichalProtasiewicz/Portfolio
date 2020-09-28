095284e16345fa65b4bcb715a96aca7defd8b55f
Michał Protasiewicz <protez95@gmail.com>
2020-09-28 12:59:00 +0200

    deploy

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { routes } from 'routes';
 import MainTemplate from 'templates/MainTemplate';
 import MainPage from 'views/MainPage';
@@ -12,11 +12,12 @@ const Root = () => (
   <BrowserRouter>
     <MainTemplate>
       <Switch>
-        <Route exact path={routes.home} component={MainPage}/>
+        <Route exact path={routes.home} component={MainPage} />
         <Route path={routes.about} component={AboutPage} />
         <Route path={routes.skills} component={SkillsPage} />
         <Route exact path={routes.projects} component={ProjectsPage} />
         <Route exact path={routes.contact} component={ContactPage} />
+        <Route render={() => <Redirect to={routes.home} />} />
       </Switch>
     </MainTemplate>
   </BrowserRouter>
